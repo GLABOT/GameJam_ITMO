a53a9a8273219ab5162d3dffada1576518af3f11
GospodinBabuin <goroprogger@gmail.com>
2023-03-19 08:51:27 +0300

    upgrade game logic

--- Assets/ButtonCombinationsMiniGame/Scripts/Indicators.cs
+++ Assets/ButtonCombinationsMiniGame/Scripts/Indicators.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +11,8 @@ public class Indicators : MonoBehaviour
     [SerializeField] private Sprite m_TexturePrefabBlue;
     [SerializeField] private Sprite m_TexturePrefabEmpty;
 
+    [SerializeField] private float m_HideIndicatorsTime = 3f;
+
     public void SetIndicatorsColor(List<Ingredients> ingredients)
     {
         for (int i = 0; i < ingredients.Count; i++)
@@ -18,6 +21,18 @@ public class Indicators : MonoBehaviour
             else if (ingredients[i] == Ingredients.Green) m_Images[i].sprite = m_TexturePrefabGreen;
             else if (ingredients[i] == Ingredients.Blue) m_Images[i].sprite = m_TexturePrefabBlue;
             else m_Images[i].sprite = m_TexturePrefabEmpty;
+        }
+
+        StartCoroutine(HideIndicators());
+    }
+
+    private IEnumerator HideIndicators()
+    {
+        yield return new WaitForSeconds(m_HideIndicatorsTime);
+
+        foreach (Image image in m_Images)
+        {
+            image.sprite = m_TexturePrefabEmpty;
         }
     }
 }
