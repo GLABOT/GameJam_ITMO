3483eea8061e2e515dce05afcd775ff3452e0e59
GospodinBabuin <goroprogger@gmail.com>
2023-03-19 08:41:34 +0300

    add sprites

--- Assets/ButtonCombinationsMiniGame/Scripts/Indicators.cs
+++ Assets/ButtonCombinationsMiniGame/Scripts/Indicators.cs
@@ -5,15 +5,19 @@ using UnityEngine.UI;
 public class Indicators : MonoBehaviour
 {
     [SerializeField] private Image[] m_Images;
+    [SerializeField] private Sprite m_TexturePrefabRed;
+    [SerializeField] private Sprite m_TexturePrefabGreen;
+    [SerializeField] private Sprite m_TexturePrefabBlue;
+    [SerializeField] private Sprite m_TexturePrefabEmpty;
 
     public void SetIndicatorsColor(List<Ingredients> ingredients)
     {
         for (int i = 0; i < ingredients.Count; i++)
         {
-            if (ingredients[i] == Ingredients.Red) m_Images[i].color = Color.red;
-            else if (ingredients[i] == Ingredients.Green) m_Images[i].color = Color.green;
-            else if (ingredients[i] == Ingredients.Blue) m_Images[i].color = Color.blue;
-            else m_Images[i].color = Color.clear;
+            if (ingredients[i] == Ingredients.Red) m_Images[i].sprite = m_TexturePrefabRed;
+            else if (ingredients[i] == Ingredients.Green) m_Images[i].sprite = m_TexturePrefabGreen;
+            else if (ingredients[i] == Ingredients.Blue) m_Images[i].sprite = m_TexturePrefabBlue;
+            else m_Images[i].sprite = m_TexturePrefabEmpty;
         }
     }
 }
